10154c0ecda969f34f84e1dd13472f9dc467f6b3
supsystic.com <supsystic.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 17:32:33 +0000

    version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/lightbox-by-supsystic/trunk@1159009 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lbs.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name: Lightbox by Supsystic
+ * Plugin URI: https://supsystic.com
+ * Description: Nice solution to add responsive lightbox (overlay) to display images, videos and galleries. Attract users with cool effects.
+ * Version: 1.0.3
+ * Author: supsystic.com
+ * Author URI: https://supsystic.com
+ **/
+	/**
+	 * Base config constants and functions
+	 */
+    require_once(dirname(__FILE__). DIRECTORY_SEPARATOR. 'config.php');
+    require_once(dirname(__FILE__). DIRECTORY_SEPARATOR. 'functions.php');
+	/**
+	 * Connect all required core classes
+	 */
+    importClassLbs('dbLbs');
+    importClassLbs('installerLbs');
+    importClassLbs('baseObjectLbs');
+    importClassLbs('moduleLbs');
+    importClassLbs('modelLbs');
+    importClassLbs('viewLbs');
+    importClassLbs('controllerLbs');
+    importClassLbs('helperLbs');
+    importClassLbs('dispatcherLbs');
+    importClassLbs('fieldLbs');
+    importClassLbs('tableLbs');
+    importClassLbs('frameLbs');
+	/**
+	 * @deprecated since version 1.0.1
+	 */
+    importClassLbs('langLbs');
+    importClassLbs('reqLbs');
+    importClassLbs('uriLbs');
+    importClassLbs('htmlLbs');
+    importClassLbs('responseLbs');
+    importClassLbs('fieldAdapterLbs');
+    importClassLbs('validatorLbs');
+    importClassLbs('errorsLbs');
+    importClassLbs('utilsLbs');
+    importClassLbs('modInstallerLbs');
+	importClassLbs('installerDbUpdaterLbs');
+	importClassLbs('dateLbs');
+	/**
+	 * Check plugin version - maybe we need to update database, and check global errors in request
+	 */
+    installerLbs::update();
+    errorsLbs::init();
+    /**
+	 * Start application
+	 */
+    frameLbs::_()->parseRoute();
+    frameLbs::_()->init();
+    frameLbs::_()->exec();
